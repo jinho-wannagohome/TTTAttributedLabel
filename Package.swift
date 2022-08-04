c7397252a586c85ce81d23718a2829d1d01d08c6
jinho-wannagohome <jinho.jang@wesang.com>
2022-08-05 01:06:01 +0900

    support spm

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TTTAttributedLabel",
+    products: [
+        .library(
+            name: "TTTAttributedLabel",
+            targets: ["TTTAttributedLabel"]),
+    ],
+    targets: [
+        .target(
+            name: "TTTAttributedLabel",
+            path: "TTTAttributedLabel"
+        )
+    ]
+)
